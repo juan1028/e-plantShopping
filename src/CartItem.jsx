012059875294ef660a0ee53e694a2c030fe32690
juan1028 <juan1028@gmail.com>
2025-07-29 20:10:18 -0500

    Final Project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach((item) => {
+      totalAmount += item.quantity * parseFloat(item.cost.substring(1));
+    
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    else {
+      dispatch(removeItem(item.name));
+    } 
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let TotalCost=0;
+    TotalCost = item.quantity * parseFloat(item.cost.substring(1));
+    return TotalCost;
+  };
+
+  const handleCheckout = () => {
+    // Logic for handling checkout can be added here
+    alert("Functionality to be added for future reference");
   };
 
   return (
